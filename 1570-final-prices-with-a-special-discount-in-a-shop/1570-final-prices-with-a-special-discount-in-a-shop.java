be6603539a39c33dbeee6aac85385f12e225f05d
shubhamsaraf09 <shubham09saraf@gmail.com>
2025-07-30 13:39:47 +0530

    Time: 1 ms (99.09%), Space: 44.2 MB (22.19%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,13 +1,30 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        Stack<Integer> stack=new Stack<>();
+        //method 1
+        
+        // Stack<Integer> stack=new Stack<>();
+        // for(int i=0;i<prices.length;i++){
+        //     while(!stack.isEmpty() && prices[i]<=prices[stack.peek()]){
+        //         int index=stack.pop();
+        //         prices[index]-=prices[i];
+        //     }
+        //     stack.push(i);
+        // } 
+        // return prices;
+
         for(int i=0;i<prices.length;i++){
-            while(!stack.isEmpty() && prices[i]<=prices[stack.peek()]){
-                int index=stack.pop();
-                prices[index]-=prices[i];
-            }
-            stack.push(i);
-        } 
+           prices[i]-=fun(i,prices);
+
+        }
         return prices;
+    }
+    int fun(int i,int[] arr){
+        int num=arr[i];
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[j]<=num){
+                return arr[j];
+            }
+        }
+        return 0;
     }
 }
