a19435665e28f261943eb1532147c15589a03ab4
shubhamsaraf09 <shubham09saraf@gmail.com>
2025-10-12 05:03:53 +0530

    Time: 16 ms (80.84%), Space: 44.1 MB (34.21%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,63 +1,43 @@
-// class Solution {
-//     public List<List<Integer>> fourSum(int[] nums, int target) {
-//         //List<List<Integer>> list=new ArrayList<>();
-        
-//         Set<List<Integer>> set2=new HashSet<>();
-//         for(int i=0;i<nums.length;i++){
-//             for(int j=i+1;j<nums.length;j++){
-//                 Set<Long> set=new HashSet<>();
-//                 for(int k=j+1;k<nums.length;k++){
-//                     Long n=(Long)target-((Long)nums[i]+nums[j]+nums[k]);
-//                     if(set.contains(n)){
-//                         List<Integer> l=new ArrayList<>();
-//                         l.add(nums[i]);
-//                         l.add(nums[j]);
-//                         l.add(nums[k]);
-//                         l.add((int)n);
-//                         Collections.sort(l); 
-//                         set2.add(l);
-//                     }
-//                     set.add(nums[k]);
-//                 }
-//             }
-//         }
-//         List<List<Integer>> list=new ArrayList<>();
-//         for (List<Integer> item : set2) {
-//             list.add(item);
-//         }
-
-//         return list;
-//     }
-// }
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Set<List<Integer>> set2 = new HashSet<>();
-        int n = nums.length;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                Set<Integer> seen = new HashSet<>(); // fresh for each (i,j)
-
-                for (int k = j + 1; k < n; k++) {
-                    // use long for intermediate calculation to avoid overflow
-                    long needLong = (long) target - ((long) nums[i] + nums[j] + nums[k]);
-
-                    // iff needLong fits in int, check seen (seen only contains ints from nums)
-                    if (needLong >= Integer.MIN_VALUE && needLong <= Integer.MAX_VALUE) {
-                        int need = (int) needLong;
-                        if (seen.contains(need)) {
-                            List<Integer> quad = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k], need));
-                            Collections.sort(quad);
-                            set2.add(quad);
-                        }
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        int n=nums.length;
+        for(int i=0;i<n-3;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n-2;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1;
+                    int l=n-1;
+                    while(k<l){
+                    // int sum=nums[i];
+                    // sum+=nums[j];
+                    // sum+=nums[k];
+                    // sum+=nums[l];
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum<target){
+                        k++;
                     }
-                    seen.add(nums[k]);
+                    else if(sum>target){
+                        l--;
+                    }
+                    else{
+                        List<Integer> L=new ArrayList<>();
+                        L.add(nums[i]);
+                        L.add(nums[j]);
+                        L.add(nums[k]);
+                        L.add(nums[l]);
+                        list.add(L);
+                        while(k<l && nums[k]==nums[k+1]) k++;
+                        while(k<l && nums[l]==nums[l-1]) l--;
+                        k++;
+                    l--;
+                    }
+                    
+                    }
                 }
             }
+            return list;
         }
-
-        return new ArrayList<>(set2);
+        
     }
-}
