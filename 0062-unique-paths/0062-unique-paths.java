6a6a5fb0f91f9de6891ef5b9fb256c3f87a1e2d1
shubhamsaraf09 <shubham09saraf@gmail.com>
2026-02-03 00:01:06 +0530

    Time: 1 ms (15.06%), Space: 42.2 MB (60.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,17 +1,20 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] grid=new int[m][n];  
-        for (int[] row : grid) {
-    Arrays.fill(row, -1);
-}
         return grid(grid,m-1,n-1);
     }
     int grid(int[][] grid,int m,int n){
-        if(m==0 && n==0) return 1;
-        if(m<0 || n<0) return 0;
-        if(grid[m][n]!=-1) return grid[m][n];
-         int right=grid(grid,m,n-1);
-         int down=grid(grid,m-1,n);
-         return grid[m][n]=right+down;
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                int up=0,left=0;
+                if(i==0 && j==0) grid[i][j]=1;
+                else {
+                    if(i>0) up=grid[i-1][j];
+                    if(j>0) left=grid[i][j-1];
+                    grid[i][j]=up+left;
+                }
+            }
+        }
+        return grid[m][n];
     }
 }
