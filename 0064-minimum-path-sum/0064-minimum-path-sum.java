05df00a404bd9b1dbf156ef9a53bc58cab841adc
shubhamsaraf09 <shubham09saraf@gmail.com>
2026-02-03 21:52:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int[][] dp=new int[grid.length][grid[0].length];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+        return grid(dp,grid,grid.length-1,grid[0].length-1);
+    }
+    int grid(int[][] dp,int[][] grid, int m,int n){
+        if(m==0 && n==0) return grid[m][n];
+        if(m<0 || n<0) return 400;
+        if(dp[m][n]!=-1) return dp[m][n];
+
+        int left=grid[m][n]+grid(dp,grid,m,n-1);
+        int up=grid[m][n]+grid(dp,grid,m-1,n);
+
+        return dp[m][n] = Math.min(left,up);
+    }
+}
