cf3f985470453453efdbdda8ad8a21d98e11af58
shubhamsaraf09 <shubham09saraf@gmail.com>
2026-03-20 01:59:51 +0530

    Time: 4 ms (86.13%), Space: 44.8 MB (49.36%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -4,20 +4,26 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
         }
-        if(target>sum) return 0;
+        
+        if(Math.abs(target)>sum) return 0;
         if((sum+target)%2!=0) return 0;
-        return f(nums.length-1,(sum+target)/2,nums);
+        int[][] dp=new int[nums.length][(sum+target)/2+1];
+        for(int[] i : dp){
+            Arrays.fill(i,-1);
+        }
+        return f(nums.length-1,(sum+target)/2,nums,dp);
     }
-    int f(int n,int target,int[] arr){
+    int f(int n,int target,int[] arr,int[][] dp){
         if(n==0){
             if(target==0 && arr[0]==0) return 2;
             if(arr[0]==target || target==0) return 1; 
             
             return 0;
         }
-        int nottake=f(n-1,target,arr);
+        if(dp[n][target]!=-1) return dp[n][target];
+        int nottake=f(n-1,target,arr,dp);
         int take=0;
-        if(arr[n]<=target) take=f(n-1,target-arr[n],arr);
-        return take+nottake;
+        if(arr[n]<=target) take=f(n-1,target-arr[n],arr,dp);
+        return dp[n][target]=take+nottake;
     }
 }
