9e4fd1c25c38005f7d8ef288f7ddc081003adbdc
shubhamsaraf09 <shubham09saraf@gmail.com>
2026-03-22 15:55:12 +0530

    Time: 22 ms (45.63%), Space: 46.6 MB (93.88%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -13,16 +13,32 @@ class Solution {
     //     return dp[n1][n2]=Math.max(f(n1-1,n2,text1,text2,dp),f(n1,n2-1,text1,text2,dp));
     // }
 
+    // int f(String s,String t){
+    //     int[][] dp=new int[s.length()+1][t.length()+1];
+    //     for(int i=0;i<=s.length();i++) dp[i][0]=0;
+    //     for(int i=0;i<=t.length();i++) dp[0][i]=0;
+    //     for(int i=1;i<=s.length();i++){
+    //         for(int j=1;j<=t.length();j++){
+    //                 if(s.charAt(i-1)==t.charAt(j-1)) dp[i][j]=1+dp[i-1][j-1];
+    //                 else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+    //         }
+    //     }
+    //     return dp[s.length()][t.length()];
+    // }
+
+    
     int f(String s,String t){
-        int[][] dp=new int[s.length()+1][t.length()+1];
-        for(int i=0;i<=s.length();i++) dp[i][0]=0;
-        for(int i=0;i<=t.length();i++) dp[0][i]=0;
+        int[] prev=new int[t.length()+1];
+        
+        for(int i=0;i<=t.length();i++) prev[i]=0;
         for(int i=1;i<=s.length();i++){
+            int[] curr=new int[t.length()+1];
             for(int j=1;j<=t.length();j++){
-                    if(s.charAt(i-1)==t.charAt(j-1)) dp[i][j]=1+dp[i-1][j-1];
-                    else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                    if(s.charAt(i-1)==t.charAt(j-1)) curr[j]=1+prev[j-1];
+                    else curr[j]=Math.max(prev[j],curr[j-1]);
             }
+            prev=curr;
         }
-        return dp[s.length()][t.length()];
+        return prev[t.length()];
     }
 }
