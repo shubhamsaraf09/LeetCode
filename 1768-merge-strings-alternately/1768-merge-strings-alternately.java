006d704688715dec1aaf8de779a9636945baa24c
shubhamsaraf09 <shubham09saraf@gmail.com>
2026-06-02 19:10:27 +0530

    Time: 4 ms (19.78%), Space: 44.3 MB (6.97%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -4,15 +4,14 @@ class Solution {
         int i=0;int j=0;
         String ans="";
         while(i<word1.length() && j<word2.length()){
-            if(c%2==0) {
+           
                 ans=ans+word1.charAt(i);
                 i++;
-            }
-            else {
+            
                 ans=ans+word2.charAt(j);
                 j++;
-            }
-            c++;
+        
+        
         }
         while(i<word1.length()){
             ans=ans+word1.charAt(i);
