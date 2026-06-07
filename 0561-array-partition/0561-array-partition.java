e8f994b2ba7bc536009a2b2e026aa35b438a3c72
shubhamsaraf09 <shubham09saraf@gmail.com>
2026-06-07 11:17:33 +0530

    Time: 18 ms (30.45%), Space: 49.9 MB (15.16%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -3,8 +3,8 @@ class Solution {
         Arrays.sort(nums);
         int sum=0;
         for(int i=0;i<nums.length-1;i+=2){
-            int min=Math.min(nums[i],nums[i+1]);
-            sum+=min;
+           
+            sum+=nums[i];
         }
             return sum;
     }
